e0a4e1603f796fabbecfbc489b8de5d7cc765121
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-03-27 08:11:07 +0700

    Feat: Add initial main and footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import styles from './App.module.css';
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,5 @@
+import styles from '../styles/Main.module.css';
+
+export default function Main() {
+  return <div className={styles.main}>Main section!</div>;
+}
