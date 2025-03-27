d23e6d713c9e48264ac97f43503dce5135e3e3be
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-03-27 08:40:50 +0700

    Feat: Update footer section
    Update the footer section, adding my GitHub profile, and OpenDota API link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
-  return <div className={styles.footer}>Footer section!</div>;
+  return (
+    <div className={styles.footer}>
+      <ul>
+        <li>
+          <a href="https://github.com/NaufalAndriantoNugraha" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+            <span>Naufal Andrianto Nugraha</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://docs.opendota.com/" target="_blank">
+            OpenDota API
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
