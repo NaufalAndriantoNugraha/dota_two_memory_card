a131e4fb89439429b0cac0bc788d5b1ef1ec667a
Naufal A.N. <92589371+NaufalAndriantoNugraha@users.noreply.github.com>
2025-04-01 21:25:22 +0700

    Refactor: Add parent container to the quit component
    A parent container is added to the quit component to block the whole page so the user can't tap the card behind it.

--- src/components/Quit.tsx
+++ src/components/Quit.tsx
@@ -13,28 +13,31 @@ export default function Quit({
 }: QuitProps) {
   return (
     <div
-      className={`${styles.quit} ${
+      className={`${styles.container} ${
         displaying ? styles['displaying'] : styles['not-displaying']
       }`}
     >
-      <div className={styles.prompt}>
-        <h1>Disconnect from the current game?</h1>
-        <p>
-          Are you sure you want to disconnect from the current game? If you do,
-          your current stats will reset when you start playing the game again.
-        </p>
-      </div>
-      <div className={styles.buttons}>
-        <button
-          type="button"
-          className={styles.button}
-          onClick={disconnectTheGame}
-        >
-          YES
-        </button>
-        <button type="button" className={styles.button} onClick={closePopup}>
-          NO
-        </button>
+      <div className={`${styles.quit} `}>
+        <div className={styles.prompt}>
+          <h1>Disconnect from the current game?</h1>
+          <p>
+            Are you sure you want to disconnect from the current game? If you
+            do, your current stats will reset when you start playing the game
+            again.
+          </p>
+        </div>
+        <div className={styles.buttons}>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={disconnectTheGame}
+          >
+            YES
+          </button>
+          <button type="button" className={styles.button} onClick={closePopup}>
+            NO
+          </button>
+        </div>
       </div>
     </div>
   );
